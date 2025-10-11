cd89efa748254e21c8416ca89ce6d3e9edce7743
Mcardona1396 <miguelcardona1396@gmail.com>
2025-10-10 19:14:12 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{padding: 40, fontFamily: 'sans-serif'}}>
-      <h1>Tiny CRM</h1>
-      <p>Your pipeline board will appear here once we wire up the database.</p>
-    </div>
-  );
-}
+export { default } from './board';
