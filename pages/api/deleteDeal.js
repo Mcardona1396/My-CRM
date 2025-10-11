f7b1a5c8efb954da19bdca30496330d7dacac719
Mcardona1396 <miguelcardona1396@gmail.com>
2025-10-10 19:21:22 -0600

    Create deleteDeal.js

---
+++ pages/api/deleteDeal.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method !== 'DELETE') return res.status(405).end();
+  const { id } = req.body;
+  await prisma.deal.delete({ where: { id } });
+  res.json({ ok: true });
+}
